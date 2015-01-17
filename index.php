b7e08bf8ae207b84eacd65dfcb845f8c601be32c
rendfall <rendfall@gmail.com>
2015-01-17 18:12:10 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Rendfall - Playground</title>
+
+        <link rel="stylesheet" href="css/main.css">
+            
+    <!--[if lte IE 8]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+   
+</head>
+
+<body class="">
+<span class="magic">
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget felis elit. 
+		Duis tristique odio a ipsum faucibus pharetra. Pellentesque consectetur, sem at sagittis mattis, 
+		augue nunc pretium ante, bibendum viverra elit turpis eget neque. Mauris fringilla libero quis sapien pharetra dictum. 
+		Phasellus ullamcorper condimentum nulla, a vehicula lacus varius non. Proin iaculis malesuada augue in iaculis. 
+		Aenean arcu purus, ultricies et mattis eu, semper quis dui. Mauris tincidunt, mauris at blandit scelerisque, 
+		magna leo feugiat justo, ut gravida dui nunc vel magna. Ut viverra, arcu vel bibendum semper, lacus nibh 
+		imperdiet purus, non imperdiet dui libero non turpis. Proin euismod eros et felis viverra a imperdiet augue fringilla. 
+		Maecenas nec diam nulla, quis egestas odio. Donec lacinia tellus at ante euismod hendrerit. Sed consequat metus non 
+		tellus faucibus luctus.
+	</p>
+	
+	<p>
+		Nam leo neque, pharetra eu interdum vitae, dapibus eget ligula. Cras in dolor eu ligula 
+		mattis dignissim vitae id eros. Nulla magna tortor, facilisis sed facilisis vitae, commodo 
+		vitae felis. Fusce vel ipsum erat, eget facilisis est. Etiam tortor elit, porta quis eleifend in, 
+		adipiscing vitae metus. Integer consequat pellentesque nunc sit amet dignissim. Phasellus ut metus neque, 
+		nec rhoncus mauris.
+	</p>
+	
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget felis elit. 
+		Duis tristique odio a ipsum faucibus pharetra. Pellentesque consectetur, sem at sagittis mattis, 
+		augue nunc pretium ante, bibendum viverra elit turpis eget neque. Mauris fringilla libero quis sapien pharetra dictum. 
+		Phasellus ullamcorper condimentum nulla, a vehicula lacus varius non. Proin iaculis malesuada augue in iaculis. 
+		Aenean arcu purus, ultricies et mattis eu, semper quis dui. Mauris tincidunt, mauris at blandit scelerisque, 
+		magna leo feugiat justo, ut gravida dui nunc vel magna. Ut viverra, arcu vel bibendum semper, lacus nibh 
+		imperdiet purus, non imperdiet dui libero non turpis. Proin euismod eros et felis viverra a imperdiet augue fringilla. 
+		Maecenas nec diam nulla, quis egestas odio. Donec lacinia tellus at ante euismod hendrerit. Sed consequat metus non 
+		tellus faucibus luctus.
+	</p>
+	
+	<p>
+		Nam leo neque, pharetra eu interdum vitae, dapibus eget ligula. Cras in dolor eu ligula 
+		mattis dignissim vitae id eros. Nulla magna tortor, facilisis sed facilisis vitae, commodo 
+		vitae felis. Fusce vel ipsum erat, eget facilisis est. Etiam tortor elit, porta quis eleifend in, 
+		adipiscing vitae metus. Integer consequat pellentesque nunc sit amet dignissim. Phasellus ut metus neque, 
+		nec rhoncus mauris.
+	</p>
+	
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget felis elit. 
+		Duis tristique odio a ipsum faucibus pharetra. Pellentesque consectetur, sem at sagittis mattis, 
+		augue nunc pretium ante, bibendum viverra elit turpis eget neque. Mauris fringilla libero quis sapien pharetra dictum. 
+		Phasellus ullamcorper condimentum nulla, a vehicula lacus varius non. Proin iaculis malesuada augue in iaculis. 
+		Aenean arcu purus, ultricies et mattis eu, semper quis dui. Mauris tincidunt, mauris at blandit scelerisque, 
+		magna leo feugiat justo, ut gravida dui nunc vel magna. Ut viverra, arcu vel bibendum semper, lacus nibh 
+		imperdiet purus, non imperdiet dui libero non turpis. Proin euismod eros et felis viverra a imperdiet augue fringilla. 
+		Maecenas nec diam nulla, quis egestas odio. Donec lacinia tellus at ante euismod hendrerit. Sed consequat metus non 
+		tellus faucibus luctus.
+	</p>
+	
+	<p>
+		Nam leo neque, pharetra eu interdum vitae, dapibus eget ligula. Cras in dolor eu ligula 
+		mattis dignissim vitae id eros. Nulla magna tortor, facilisis sed facilisis vitae, commodo 
+		vitae felis. Fusce vel ipsum erat, eget facilisis est. Etiam tortor elit, porta quis eleifend in, 
+		adipiscing vitae metus. Integer consequat pellentesque nunc sit amet dignissim. Phasellus ut metus neque, 
+		nec rhoncus mauris.
+	</p>
+</span>
+
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+    <script src="js/magicwand.js"></script>
+    <script src="js/widgets.js"></script>
+
+</body>
+</html>
